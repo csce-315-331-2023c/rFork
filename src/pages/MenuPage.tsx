9c3e3053087c98a20628d7c7ed3770b83be89673
Lucas-Vaughan <lucas@steeledestinies.com>
2023-10-30 17:03:25 -0500

    Template html page added for Menu

---
+++ src/pages/MenuPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MenuPage() {
+  return (
+    <div>
+        <header>
+            Testing Page
+        </header>
+        <div>
+            Testing Block
+        </div>
+    </div>
+  )
+}
