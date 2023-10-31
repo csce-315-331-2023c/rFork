971f3d7eb95ea3420d8a54a594e40e06986c16ad
Jackson Stewart <jts3304@gmail.com>
2023-10-31 11:22:36 -0500

    Fix initialization process

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,15 @@ if (process.env.NODE_ENV === 'development') {
     console.warn('Running in development mode!');
 }
 
-import { initDatabase } from './db_init';
+import { Client } from 'pg';
 
-// this runs when the file is first imported, no need to explicitly invoke it
-export const db = initDatabase();
+const client = new Client({
+    connectionString: process.env.DB_URI,
+});
+
+client.connect().catch((err) => {
+    console.error(`Failed to connect to database: ${err}`);
+    process.exit(1);
+});
+
+export default client;
