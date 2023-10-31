aa88acd4f966ee3cdb8880a23c8651cd04a2d8b8
LucientZ <koblenzpoisiden@gmail.com>
2023-10-31 02:19:18 -0500

    Added more styling to kiosk buttons

--- src/components/KioskButton.tsx
+++ src/components/KioskButton.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 type KioskButtonProps = {
     text: string,
+    price: number,
     imageURI?: string,
     onPress?: () => void,
 }
 
-export default function KioskButton({ text, imageURI, onPress }: KioskButtonProps) {
+export default function KioskButton({ text, price, imageURI, onPress }: KioskButtonProps) {
     return (
-        <button className='flex-row bg-slate-600' onClick={onPress ?? (() => console.log("Button Pressed"))}>
-            {text}
+        <button className='flex-col justify-evenly items-center rounded-xl py-4 text-2xl h-60 hover:bg-slate-200 bg-slate-50' onClick={onPress ?? (() => console.log("Button Pressed"))}>
+            <p className='absolute'>{`$${(price / 100).toFixed(2)}`}</p>
+            <img alt={text} className='object-scale-down w-auto max-h-44' src='https://static.vecteezy.com/system/resources/previews/025/065/282/original/crepe-with-ai-generated-free-png.png' />
+            <p>{text}</p>
         </button>
     )
 }
--- src/pages/Kiosk.tsx
+++ src/pages/Kiosk.tsx
@@ -1,17 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../css/index.css"
 import KioskButton from '../components/KioskButton'
+import { MenuItem } from '../types';
 
 export default function Kiosk() {
+
+  const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+
+  useEffect(() => {
+    const fakeData: MenuItem[] = [
+      {
+        id: 0,
+        name: "Crepe",
+        price: 100,
+        ingredients: []
+      },
+      {
+        id: 0,
+        name: "Strawberry Crepe",
+        price: 200,
+        ingredients: []
+      },
+      {
+        id: 0,
+        name: "Lemon Crepe",
+        price: 100,
+        ingredients: []
+      },
+      {
+        id: 0,
+        name: "Chicken Enchilada",
+        price: 100,
+        ingredients: []
+      },
+    ];
+
+    setMenuItems(fakeData);
+  }, []);
+
   return (
     <div className='flex-col'>
       {/* Navbar */}
       <div></div>
       {/* Slide Menus */}
       <div className='px-10'>
-        <h1 className=' text-4xl'>Crepes</h1>
-        <div className='grid grid-cols-2 gap-4'>
-          <KioskButton text='asdf' />
+        <h1 className='text-4xl'>Items</h1>
+        <div className='grid grid-cols-4 gap-4'>
+          {menuItems.map((menuItem) => (
+            <KioskButton
+              text={menuItem.name}
+              price={menuItem.price}
+            />
+          ))}
         </div>
       </div>
     </div>
