3386e814aa9fdc9dd0c1f67c8ea49d25017c3ffa
LucientZ <koblenzpoisiden@gmail.com>
2023-10-31 20:36:56 -0500

    Fixed routes

--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -1,10 +1,10 @@
 import { getAllMenuItems } from '../../../api'
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server'
 
 export async function GET() {
     return getAllMenuItems().then((result) => {
-        return NextResponse.json(result, {status: 200});
+        return NextResponse.json(result, { status: 200 });
     }).catch((error) => {
-        return NextResponse.json({ message: "Something went wrong", error }, {status: 500});
+        return NextResponse.json({ message: "Something went wrong", error }, { status: 500 });
     })
 }
