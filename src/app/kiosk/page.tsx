3c8f9817eaa902db8da442ce46c8d16faa3ed9a4
LucientZ <koblenzpoisiden@gmail.com>
2023-11-13 23:09:41 -0600

    Finished basic nav for kiosk

--- src/app/kiosk/page.tsx
+++ src/app/kiosk/page.tsx
@@ -4,38 +4,81 @@ import "../../css/index.css"
 import KioskButton from '../../components/ImageButton'
 import { MenuItem } from '../../types';
 import PageLoading from '../../components/PageLoading';
+import TextButton from '../../components/TextButton';
+import Link from 'next/link';
+import { Modal } from 'react-bootstrap';
 
 export default function Kiosk() {
-    const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
-        fetch("/api/menu").then(async (result) => {
-            const allMenuItems = await result.json();
-            setMenuItems(allMenuItems);
-            setLoading(false);
-        });
+        setLoading(false);
     }, []);
 
     if (loading) return <PageLoading />;
 
+    const enum Direction {
+        LEFT,
+        RIGHT,
+        BOTH,
+        NONE,
+    }
+
+    function KioskNavButton({ text, borderDirection, onPress }: { text: string, borderDirection?: Direction, onPress: () => any }): React.JSX.Element {
+        const [extraClasses, setExtraClasses] = useState<string>("");
+        const [borderClasses, setBorderClasses] = useState<string>("");
+
+        useEffect(() => {
+            switch (borderDirection) {
+                case Direction.LEFT:
+                    setBorderClasses("border-black border-l-2");
+                    break;
+                case Direction.RIGHT:
+                    setBorderClasses("border-black border-r-2");
+                    break;
+                case Direction.BOTH:
+                    setBorderClasses("border-black border-x-2");
+                    break;
+                default:
+                    break;
+            }
+        }, [borderDirection]);
+
+        return (
+            <button
+                onClick={onPress}
+                className={`border-gray-700 px-4 h-full text-xl ${extraClasses} ${borderClasses}`}
+                onMouseEnter={() => setExtraClasses("underline")}
+                onMouseLeave={() => setExtraClasses("")}
+            >
+                {text}
+            </button>
+        );
+    }
+
     return (
         <div className='flex-col'>
             {/* Navbar */}
-            <div></div>
+            <nav className='flex flex-row items-center justify-start p-6 w-full bg-red-500'>
+                <img src='https://www.sweetparis.com/assets/logos/sweet-paris-logo.svg' className='max-h-16 mr-6' />
+                <KioskNavButton onPress={() => { }} borderDirection={Direction.RIGHT} text='Sweet Crepes' />
+                <KioskNavButton onPress={() => { }} borderDirection={Direction.RIGHT} text='Savory Crepes' />
+                <KioskNavButton onPress={() => { }} borderDirection={Direction.RIGHT} text='Waffles' />
+                <KioskNavButton onPress={() => { }} borderDirection={Direction.RIGHT} text='Soups' />
+                <KioskNavButton onPress={() => { }} borderDirection={Direction.NONE} text='Drinks' />
+                <div className='flex-1 flex flex-row justify-end'>
+                    <TextButton text='Checkout' customClassName='h-[50%] rounded-full p-4 transition-colors delay-100' color='#007DDD' hoverColor='#0200FF' textColor='white' />
+                </div>
+            </nav>
             {/* Slide Menus */}
             <div className='px-10'>
-                <h1 className='text-4xl'>Items</h1>
                 <div className='grid grid-cols-4 gap-4'>
-                    {menuItems.map((menuItem, index) => (
-                        <KioskButton
-                            text={menuItem.name}
-                            price={menuItem.price}
-                            key={index}
-                        />
-                    ))}
                 </div>
             </div>
         </div>
     )
+}
+
+function SweetCrepesContent() {
+
 }
--- src/components/TextButton.tsx
+++ src/components/TextButton.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
 
-export default function TextButton({ text, onPress, color, hoverColor, textColor }:
+export default function TextButton({ text, onPress, color, hoverColor, textColor, customClassName }:
     {
         text?: string,
         onPress?: () => void,
         color?: string,
         hoverColor?: string,
         textColor?: string,
+        customClassName?: string,
     }) {
     const [hover, setHover] = useState<boolean>();
 
     return (
         <button
-            className='h-full w-full text-center text-lg rounded-xl drop-shadow-md'
+            className={customClassName ? customClassName : 'h-full w-full text-center text-lg rounded-xl drop-shadow-md'}
             onClick={onPress ?? (() => console.log("Button Pressed"))}
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
