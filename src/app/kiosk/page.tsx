97192178a6219fcf53f006cd313d6a4ff2eb5a42
LucientZ <koblenzpoisiden@gmail.com>
2023-11-14 20:49:17 -0600

    Added color utilities and kiosk order submission screen

--- src/app/kiosk/page.tsx
+++ src/app/kiosk/page.tsx
@@ -1,24 +1,16 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import "../../css/index.css"
-import KioskButton from '../../components/ImageButton'
+import ImageButton from '../../components/ImageButton'
 import { MenuItem, Order } from '../../types';
 import PageLoading from '../../components/PageLoading';
 import TextButton from '../../components/TextButton';
-import Link from 'next/link';
 import Popup from '../../components/Popup';
 
 export default function Kiosk() {
     // User flow hooks
     const [loading, setLoading] = useState<boolean>(true);
     const [showCustomizationPopup, setShowCustomizationPopup] = useState<boolean>(true);
-
-    // Lists of different tagged menu items
-    const [sweetCrepes, setSweetCrepes] = useState<Array<MenuItem>>([]);
-    const [savoryCrepes, setSavoryCrepes] = useState<Array<MenuItem>>([]);
-    const [waffles, setWaffles] = useState<Array<MenuItem>>([]);
-    const [soups, setSoups] = useState<Array<MenuItem>>([]);
-    const [drinks, setDrinks] = useState<Array<MenuItem>>([]);
 
     const testMenuItem: MenuItem = {
         name: "[MENU ITEM]",
@@ -27,10 +19,20 @@ export default function Kiosk() {
         validExtras: [],
     }
 
-    // Data for adding item to cart
-    const [selectedItem, setSelectedItem] = useState<MenuItem>(testMenuItem);
+    // Lists of different tagged menu items
+    const [sweetCrepes, setSweetCrepes] = useState<Array<MenuItem>>([testMenuItem]);
+    const [savoryCrepes, setSavoryCrepes] = useState<Array<MenuItem>>([]);
+    const [waffles, setWaffles] = useState<Array<MenuItem>>([]);
+    const [soups, setSoups] = useState<Array<MenuItem>>([]);
+    const [drinks, setDrinks] = useState<Array<MenuItem>>([]);
+
+
+    // Temporary/Volatile data for user selecting experience
+    const [selectedItemList, setSelectedItemList] = useState<Array<MenuItem>>(sweetCrepes);
+    const [selectedItem, setSelectedItem] = useState<MenuItem | undefined>();
 
     // Final order info data
+    const [showCheckoutPopup, setShowCheckoutPopup] = useState<boolean>(false);
     const [orderInfo, setOrderInfo] = useState<Order>();
     const [cartItems, setCartItems] = useState<Array<MenuItem>>([]);
 
@@ -81,36 +83,55 @@ export default function Kiosk() {
 
     function addToOrder(menuItem: MenuItem) {
         const updatedCartItems = cartItems;
-        updatedCartItems.push(selectedItem);
+        updatedCartItems.push(menuItem);
         setCartItems(updatedCartItems);
         console.log(cartItems);
     }
 
     return (
         <div className='flex flex-col h-screen'>
+
             {/* Navbar */}
             <header className='h-[10%] w-full flex flex-row items-center justify-start px-6 py-2 bg-[#d6e3ff]'>
                 <img src='https://www.sweetparis.com/assets/logos/sweet-paris-logo.svg' className='max-h-16 mr-6' />
-                <KioskNavButton onPress={() => { }} borderDirection={Direction.RIGHT} text='Sweet Crepes' />
-                <KioskNavButton onPress={() => { }} borderDirection={Direction.RIGHT} text='Savory Crepes' />
-                <KioskNavButton onPress={() => { }} borderDirection={Direction.RIGHT} text='Waffles' />
-                <KioskNavButton onPress={() => { }} borderDirection={Direction.RIGHT} text='Soups' />
-                <KioskNavButton onPress={() => { }} borderDirection={Direction.NONE} text='Drinks' />
+                <KioskNavButton onPress={() => setSelectedItemList(sweetCrepes)} borderDirection={Direction.RIGHT} text='Sweet Crepes' />
+                <KioskNavButton onPress={() => setSelectedItemList(savoryCrepes)} borderDirection={Direction.RIGHT} text='Savory Crepes' />
+                <KioskNavButton onPress={() => setSelectedItemList(waffles)} borderDirection={Direction.RIGHT} text='Waffles' />
+                <KioskNavButton onPress={() => setSelectedItemList(soups)} borderDirection={Direction.RIGHT} text='Soups' />
+                <KioskNavButton onPress={() => setSelectedItemList(drinks)} borderDirection={Direction.NONE} text='Drinks' />
             </header>
+
             {/* Menu Content */}
             <div className='px-10 py-2 flex-1 h-4 overflow-y-auto'>
                 <h1 className='text-4xl underline'>Items</h1>
                 <div className='grid grid-cols-4 gap-4'>
-
+                    {selectedItemList.map((menuItem) => {
+                        return (
+                            <ImageButton
+                                key={menuItem.name}
+                                text={menuItem.name}
+                                onPress={() => {
+                                    setSelectedItem(menuItem);
+                                    setShowCustomizationPopup(true);
+                                }}
+                            />
+                        )
+                    })}
                 </div>
             </div>
+
             {/* Bottom Buttons */}
             <footer className='h-[10%]'>
                 <div className='flex flex-row items-center h-full'>
-                    <TextButton customClassName='flex-1 py-2 h-full' text='Cancel Order' />
-                    <TextButton customClassName='flex-1 py-2 h-full' text='Checkout' />
+                    <TextButton customClassName='flex-1 py-2 h-full text-2xl' text='Cancel Order' />
+                    <TextButton
+                        customClassName='flex-1 py-2 h-full text-2xl'
+                        text='Checkout'
+                        onPress={() => setShowCheckoutPopup(true)}
+                    />
                 </div>
             </footer>
+
             {/* Popups */}
             <Popup
                 show={showCustomizationPopup}
@@ -128,7 +149,7 @@ export default function Kiosk() {
                         />
                     </header>
                     <div className='flex-1 grid grid-cols-3 grid-rows-6 gap-2 p-2'>
-                        {selectedItem.ingredients.map((ingredient) => {
+                        {selectedItem && selectedItem.ingredients.map((ingredient) => {
                             return (
                                 <TextButton key={ingredient.itemId} text={`Remove ${ingredient.itemId}`} />
                             )
@@ -139,7 +160,29 @@ export default function Kiosk() {
                         <TextButton
                             text='Add to Order'
                             onPress={() => {
-                                addToOrder(selectedItem);
+                                if (selectedItem) {
+                                    addToOrder(selectedItem);
+                                }
+                                setShowCustomizationPopup(false);
+                            }}
+                            color='#FF9638'
+                            hoverColor='#FFC38E'
+                        />
+                    </footer>
+                </div>
+            </Popup>
+            <Popup
+                show={showCheckoutPopup}
+            >
+                <div className='h-full w-full bg-white'>
+                    <footer className='h-[10%] flex flex-row p-2 gap-10'>
+                        <TextButton text='Back' onPress={() => setShowCheckoutPopup(false)} />
+                        <TextButton
+                            text='Submit'
+                            onPress={() => {
+                                if (selectedItem) {
+                                    addToOrder(selectedItem);
+                                }
                                 setShowCustomizationPopup(false);
                             }}
                             color='#FF9638'
