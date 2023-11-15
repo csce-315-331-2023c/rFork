c2b087671669a38eda23603630aefa0c94811222
Jackson Stewart <jts3304@gmail.com>
2023-11-14 23:18:06 -0600

    Add search by tag function

--- src/api/index.ts
+++ src/api/index.ts
@@ -18,5 +18,5 @@ export default client;
 
 // export everything so that it can be imported from a single file
 export { getAllInventoryItems } from './inventory';
-export { getAllMenuItems } from './menu';
+export { getAllMenuItems, getMenuItemByTag } from './menu';
 export { submitOrder } from './order';
