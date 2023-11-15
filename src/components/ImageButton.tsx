aa55209426007d0cc01567bbf86e06e71690fc7c
LucientZ <koblenzpoisiden@gmail.com>
2023-11-15 14:56:09 -0600

    Implemented basic manager dashboard

--- src/components/ImageButton.tsx
+++ src/components/ImageButton.tsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react'
 
 
-export default function ImageButton({ text, price, imageURI, onPress, color, hoverColor, altText }: {
-    text: string,
+export default function ImageButton({ text, price, imageURI, onPress, color, hoverColor, altText, customClassName }: {
+    text?: string,
     altText?: string,
     price?: number,
     imageURI?: string,
     onPress?: () => void,
     color?: string
     hoverColor?: string,
+    customClassName?: string,
 }) {
 
     const [hover, setHover] = useState<boolean>(false);
     return (
         <button
-            className='flex flex-col justify-evenly items-center rounded-xl py-4 text-2xl h-60'
+            className={customClassName ?? 'flex flex-col justify-evenly items-center rounded-xl py-4 text-2xl h-60'}
             onClick={onPress ?? (() => console.log("Button Pressed"))}
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
@@ -27,7 +28,7 @@ export default function ImageButton({ text, price, imageURI, onPress, color, hov
                 className='object-scale-down w-auto max-h-44'
                 src={imageURI ?? 'https://static.vecteezy.com/system/resources/previews/025/065/282/original/crepe-with-ai-generated-free-png.png'}
             />
-            <p>{`${text}${price ? ` - $${(price).toFixed(2)}` : ''}`}</p>
+            <p>{text ? `${text}${price ? ` - $${(price).toFixed(2)}` : ''}` : ''}</p>
             <p className=''>{ }</p>
         </button>
     )
