55d69242f5bd54da2f29069f9d81895b04ee0f90
ShahirAliGit <shahirali@tamu.edu>
2023-12-03 17:25:06 -0600

    finsihed backend for product sales report

--- src/api/reports.ts
+++ src/api/reports.ts
@@ -1,3 +1,51 @@
 import React from 'react'
 import 'dotenv/config';
+import db from "./index";
+import { InventoryItem, Order } from "../types";
+
+export async function product_usage(){
+
+}
+
+export async function product_sales(start: Date, end: Date): Promise<Order[]>{
+    const query = 'SELECT row_to_json(t) FROM (SELECT * FROM orders WHERE create_time BETWEEN $1 AND $2 ORDER BY create_time DESC) t';
+    const result = await db.query(query, [start, end]);
+
+    let orders: Order[] = [];
+    for (let row of result.rows) {
+        const { 
+            f1: id, 
+            f2: create_time, 
+            f3: total,
+            f4: submitted_by
+        } = row.row_to_json.row;
+        
+        orders.push({
+            id: id,
+            timestamp: create_time,
+            total: total,
+            submittedBy: submitted_by,
+            items: []
+        });
+    }
+
+    return orders;
+}
+
+export async function product_excess(){
+
+}
+
+export async function restock_report(): Promise<InventoryItem[]>{
+
+}
+
+export async function sells_together(start: Date, end: Date): Promise<Order[]>{
+    
+}
+
+
+
+
+
 
