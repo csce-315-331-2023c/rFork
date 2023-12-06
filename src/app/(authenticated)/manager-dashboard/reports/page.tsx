11634bb78a5c7d9ea2a4172a0e0992f4ba4bd207
ShahirAliGit <shahirali@tamu.edu>
2023-12-05 21:51:12 -0600

    finished loading for transaction history and reports, as well as a retry button for reports

--- src/app/(authenticated)/manager-dashboard/reports/page.tsx
+++ src/app/(authenticated)/manager-dashboard/reports/page.tsx
@@ -5,6 +5,8 @@ import "../../../../css/index.css"
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import AuthSessionHeader from '../../../../components/AuthSessionHeader';
+import 'bootstrap/dist/css/bootstrap.css'
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function ReportView() {
 
@@ -17,10 +19,14 @@ export default function ReportView() {
     }
 
     const [reportType, setReportType] = useState<r>(r.product_usage);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [empty, setEmpty] = useState<boolean>(false);
 
     useEffect(() => {
         fetch('/api/reports?reports=product_usage&start=0&end=1701660672190').then(res => res.json()).then(d => {
+            setLoading(false);
             setData(d);
+            setEmpty(d.length == 0);
         })
     }, []);
 
@@ -29,44 +35,76 @@ export default function ReportView() {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
 
+
+
     return (
         <div>
             <AuthSessionHeader />
             <div className="grid grid-cols-3 gap-4 p-4">
                 <div className="flex flex-col items-start p-4">
                     <button aria-label="Product Usage" className="shadow-lg rounded-lg border-blue-300 border-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-16 px-4 rounded mb-2 w-full" onClick={() => {
+                        setLoading(true);
                         fetch(`/api/reports?reports=product_usage&start=${startDate.getTime()}&end=${endDate.getTime()}`).then(res => res.json()).then(d => {
+                            setLoading(false);
                             setData(d);
+                            setEmpty(d.length == 0);
                             setReportType(r.product_usage);
                         })
                     }}>Product Usage</button>
                     <button aria-label="Product Sales" className="shadow-lg rounded-lg border-blue-300 border-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-16 px-4 rounded mb-2 w-full" onClick={() => {
+                        setLoading(true);
                         fetch(`/api/reports?reports=product_sales&start=${startDate.getTime()}&end=${endDate.getTime()}`).then(res => res.json()).then(d => {
+                            setLoading(false);
                             setData(d);
+                            setEmpty(d.length == 0);
                             setReportType(r.product_sales);
                         })
                     }}>Product Sales</button>
                     <button aria-label="Product Excess" className="shadow-lg rounded-lg border-blue-300 border-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-16 px-4 rounded mb-2 w-full" onClick={() => {
+                        setLoading(true);
                         fetch(`/api/reports?reports=product_excess&start=${startDate.getTime()}`).then(res => res.json()).then(d => {
+                            setLoading(false);
                             setData(d);
+                            setEmpty(d.length == 0);
                             setReportType(r.product_excess);
                         })
                     }}>Product Excess</button>
                     <button aria-label="Restock Report" className="shadow-lg rounded-lg border-blue-300 border-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-16 px-4 rounded mb-2 w-full" onClick={() => {
+                        setLoading(true);
                         fetch(`/api/reports?reports=restock_report`).then(res => res.json()).then(d => {
+                            setLoading(false);
                             setData(d);
+                            setEmpty(d.length == 0);
                             setReportType(r.restock_report);
                         })
                     }}>Restock Report</button>
                     <button aria-label="Sells Together" className="shadow-lg rounded-lg border-blue-300 border-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-16 px-4 rounded mb-2 w-full" onClick={() => {
+                        setLoading(true);
                         fetch(`/api/reports?reports=sells_together&start=${startDate.getTime()}&end=${endDate.getTime()}`).then(res => res.json()).then(d => {
+                            setLoading(false);
                             setData(d);
+                            setEmpty(d.length == 0);
                             setReportType(r.sells_together);
                         })
                     }}>Sells Together</button>
                 </div>
                 {
-                    reportType == r.product_usage &&
+                    empty &&
+                    <div className="flex flex-col items-center p-4">
+                        No Data
+                    </div>
+                }
+                {
+                    loading &&
+                    <div className="flex flex-col items-center p-4">
+                        <Spinner animation="border" role="status" className="item-center w-full">
+                            <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                        Loading...
+                    </div>
+                }
+                {
+                    reportType == r.product_usage && !empty && !loading &&
                     <div>
                         <table>
                             <thead>
@@ -93,7 +131,7 @@ export default function ReportView() {
                     </div>
                 }
                 {
-                    reportType == r.product_sales &&
+                    reportType == r.product_sales && !empty && !loading &&
                     <div>
                         <table>
                             <thead>
@@ -122,7 +160,7 @@ export default function ReportView() {
                     </div>
                 }
                 {
-                    reportType == r.product_excess &&
+                    reportType == r.product_excess && !empty && !loading &&
                     <div>
                         <table>
                             <thead>
@@ -152,7 +190,7 @@ export default function ReportView() {
                     </div>
                 }
                 {
-                    reportType == r.restock_report &&
+                    reportType == r.restock_report && !empty && !loading &&
                     <div>
                         <table>
                             <thead>
@@ -182,7 +220,7 @@ export default function ReportView() {
                     </div>
                 }
                 {
-                    reportType == r.sells_together &&
+                    reportType == r.sells_together && !empty && !loading &&
                     <div>
                         <table>
                             <thead>
@@ -209,12 +247,67 @@ export default function ReportView() {
                         </table>
                     </div>
                 }
-                <div className="flex flex-col">
-                    <h2 className="text-left py-3 text-2xl">Start Date</h2>
-                    <DatePicker isClearable placeholderText="Select Start Date" selected={startDate} onChange={(date) => setStartDate(date as Date)}>Start Date</DatePicker>
-                    <h2 className="text-left py-3 pt-8 text-2xl">End Date</h2>
-                    <DatePicker isClearable placeholderText="Select End Date" selected={endDate} onChange={(date) => setEndDate(date as Date)}>End Date</DatePicker>
-                </div>
+                { (reportType == r.product_usage || reportType == r.product_sales || reportType == r.product_excess ||reportType == r.sells_together) && 
+                    <div className="flex flex-col items-center">
+                        <div className="w-full">
+                            <h2 className="text-left py-3 text-2xl">Start Date</h2>
+                            <DatePicker isClearable placeholderText="Select Start Date" selected={startDate} onChange={(date) => setStartDate(date as Date)}>Start Date</DatePicker>
+                            {
+                                reportType != r.product_excess &&
+                                <div>
+                                    <h2 className="text-left py-3 pt-8 text-2xl">End Date</h2>
+                                    <DatePicker isClearable placeholderText="Select End Date" selected={endDate} onChange={(date) => setEndDate(date as Date)}>End Date</DatePicker>
+                                </div>
+                            }
+                        </div>
+                        <button
+                            aria-label="Serach Again"
+                            className="shadow-lg rounded-lg border-blue-300 border-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 mx-8 rounded mb-2 w-full"
+                            onClick={() => {
+                                switch (reportType) {
+                                    case r.product_usage:
+                                        setLoading(true);
+                                        fetch(`/api/reports?reports=product_usage&start=${startDate.getTime()}&end=${endDate.getTime()}`).then(res => res.json()).then(d => {
+                                            setLoading(false);
+                                            setData(d);
+                                            setEmpty(d.length == 0);
+                                            setReportType(r.product_usage);
+                                        })
+                                        break;
+                                    case r.product_sales:
+                                        setLoading(true);
+                                        fetch(`/api/reports?reports=product_sales&start=${startDate.getTime()}&end=${endDate.getTime()}`).then(res => res.json()).then(d => {
+                                            setLoading(false);
+                                            setData(d);
+                                            setEmpty(d.length == 0);
+                                            setReportType(r.product_sales);
+                                        })
+                                        break;
+                                    case r.product_excess:
+                                        setLoading(true);
+                                        fetch(`/api/reports?reports=restock_report`).then(res => res.json()).then(d => {
+                                            setLoading(false);
+                                            setData(d);
+                                            setEmpty(d.length == 0);
+                                            setReportType(r.restock_report);
+                                        })
+                                        break;
+                                    case r.sells_together:
+                                        setLoading(true);
+                                        fetch(`/api/reports?reports=sells_together&start=${startDate.getTime()}&end=${endDate.getTime()}`).then(res => res.json()).then(d => {
+                                            setLoading(false);
+                                            setData(d);
+                                            setEmpty(d.length == 0);
+                                            setReportType(r.sells_together);
+                                        })
+                                        break;
+                                }
+                            }}
+                        >
+                            Search Again
+                        </button>
+                    </div>
+                }
             </div>
         </div>
     )
--- src/app/(authenticated)/manager-dashboard/transaction-history/page.tsx
+++ src/app/(authenticated)/manager-dashboard/transaction-history/page.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react'
 import { getAll, getNotFinished } from '../../../../api/order'
 import AuthSessionHeader from '../../../../components/AuthSessionHeader';
 import "../../../../css/index.css"
+import 'bootstrap/dist/css/bootstrap.css'
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function TranscationHistory() {
     enum t {
@@ -11,9 +13,11 @@ export default function TranscationHistory() {
     }
 
     const [historyType, setHistoryType] = useState(t.getAll)
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
-        fetch('/api/orders?history=getAll').then(res => res.json()).then(d => {
+        fetch('/api/orders?history=getNotFinished').then(res => res.json()).then(d => {
+            setLoading(false);
             setData(d);
         })
     }, []);
@@ -25,21 +29,34 @@ export default function TranscationHistory() {
             <AuthSessionHeader />
             <div className="grid grid-cols-2 gap-4 p-4">
                 <div className="flex flex-col items-start p-4">
+                <button aria-label="Get In Flight Orders" className="shadow-lg rouded-lg border-blue-300 border-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-16 px-4 rounded mb-2 w-full" onClick={() => {
+                        setLoading(true);
+                        fetch(`/api/orders?history=getNotFinished`).then(res => res.json()).then(d => {
+                            setLoading(false);
+                            setData(d);
+                            setHistoryType(t.getNotFinished);
+                        })
+                }}>Get In Flight Orders</button>
                     <button aria-label="Get All Orders" className="shadow-lg rounded-lg border-blue-300 border-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-16 px-4 rounded mb-2 w-full" onClick={() => {
+                        setLoading(true);
                         fetch(`/api/orders?history=getAll`).then(res => res.json()).then(d => {
+                            setLoading(false);
                             setData(d);
                             setHistoryType(t.getAll);
                         })
                     }}>Get All Orders</button>
-                    <button aria-label="Get In Flight Orders" className="shadow-lg rouded-lg border-blue-300 border-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-16 px-4 rounded mb-2 w-full" onClick={() => {
-                        fetch(`/api/orders?history=getNotFinished`).then(res => res.json()).then(d => {
-                            setData(d);
-                            setHistoryType(t.getNotFinished);
-                        })
-                    }}>Get In Flight Orders</button>
                 </div>
                 {
-                    historyType == t.getAll &&
+                    loading &&
+                    <div className="flex flex-col items-center p-4">
+                        <Spinner animation="border" role="status" className="item-center w-full">
+                            <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                        Loading...
+                    </div>
+                }
+                {
+                    historyType == t.getAll && !loading &&
 
                     <table>
                         <thead>
@@ -70,7 +87,7 @@ export default function TranscationHistory() {
 
                 }
                 {
-                    historyType == t.getNotFinished &&
+                    historyType == t.getNotFinished && !loading &&
 
                     <table>
                         <thead>
