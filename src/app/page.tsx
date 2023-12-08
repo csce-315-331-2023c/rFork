aaefec739a4ddb49e51d5703ced655aed800f232
ShahirAliGit <shahirali@tamu.edu>
2023-12-08 12:48:56 -0600

    contrast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 export default function Initial() {
     // const optionsArray = Object.keys(Languages);
 
-    const clazz = "w-full h-full flex flex-col justify-center items-center text-white text-4xl"
-
     return (
         <div className="h-screen w-screen">
             <div className='grid grid-rows-2 grid-cols-2 h-full w-full'>
 
-                <Link href='/cash-register'><div className={`${clazz} bg-red-500`}>Cash Register</div></Link>
-                <Link href='/manager-dashboard'><div className={`${clazz} bg-blue-500`}>Manager Dashboard</div></Link>
-                <Link href='/kiosk'><div className={`${clazz} bg-yellow-500`}>Customer Kiosk</div></Link>
-                <Link href='/menu-board'><div className={`${clazz} bg-green-500`}>Menu Board</div></Link>
+                <Link href='/cash-register'><div className={`w-full h-full flex flex-col justify-center items-center text-white text-4xl bg-red-500`}>Cash Register</div></Link>
+                <Link href='/manager-dashboard'><div className={`w-full h-full flex flex-col justify-center items-center text-white text-4xl bg-blue-600`}>Manager Dashboard</div></Link>
+                <Link href='/kiosk'><div className={`w-full h-full flex flex-col justify-center items-center text-black text-4xl bg-yellow-200`}>Customer Kiosk</div></Link>
+                <Link href='/menu-board'><div className={`w-full h-full flex flex-col justify-center items-center text-black text-4xl bg-green-300`}>Menu Board</div></Link>
             </div>
         </div>
     )
